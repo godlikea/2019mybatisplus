a93ff9be4011c76927bae3bff1378054ef3136b4
guoguangkai <guoguangkai@example.com>
2019-03-21 17:51:05 +0800

    修改默认tomcat 容器为 undertow

--- src/main/java/com/kai/controller/LoginController.java
+++ src/main/java/com/kai/controller/LoginController.java
@@ -5,12 +5,15 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ThreadContext;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kai.model.TUser;
+
+import io.undertow.servlet.api.SessionManagerFactory;
 /**
  * 登录测试shiro
  * @author 郭广凯
@@ -33,6 +36,10 @@ public class LoginController {
 	@PostMapping("/login")
 	public Object login(TUser user) {
 		Subject subject = SecurityUtils.getSubject();
+		if((System.currentTimeMillis()-subject.getSession().getStartTimestamp().getTime())>=1000) {
+			//移除线程中的subject
+			ThreadContext.remove(ThreadContext.SUBJECT_KEY);
+		}
 		UsernamePasswordToken upt=new UsernamePasswordToken(user.getName(), user.getPwd(),true);
 		subject.login(upt);
 		return "Login";
